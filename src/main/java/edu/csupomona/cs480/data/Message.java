585f31514da866f373f796cfaa99452df54df8f6
Kevin Liu <kevin_rx_liu@yahoo.com>
2016-07-06 22:16:53 -0700

    Forgot to save earlier

--- src/main/java/edu/csupomona/cs480/data/Message.java
+++ src/main/java/edu/csupomona/cs480/data/Message.java
@@ -1,5 +1,25 @@
 package edu.csupomona.cs480.data;
 
-public class Message {
+import java.util.Date;
 
+public class Message{
+	private String id;
+	private String message;
+	private Date time;
+	
+	Message(String id, String message){
+		this.id = id;
+		this.message = message;
+		time = new Date();
+	}
+	
+	String getId(){
+		return id;
+	}
+	String getMsg(){
+		return message;
+	}
+	Date getTime(){
+		return time;
+	}
 }
--- src/main/java/edu/csupomona/cs480/data/User.java
+++ src/main/java/edu/csupomona/cs480/data/User.java
@@ -2,8 +2,7 @@ package edu.csupomona.cs480.data;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.TreeMap;
-
+import java.util.HashMap;
 
 /**
  * The basic user object.
@@ -11,44 +10,57 @@ import java.util.TreeMap;
 public class User {
 
 	/** The unique user Id */
-    private String id;
-    /** The unique user Id */
-    private String name;
-    /** The unique user Id */
-    private String major;
-    /** The timestamp when the user is being created */
-    private String creationTime = new Date(System.currentTimeMillis()).toString();
-    
-    private TreeMap friends;
-    
-    public User(){
-    }
-    
-    public User(String id, String name, String major){
-    	friends = new TreeMap <String,String>();
-    	this.id = id;
-    	this.name = name;
-    	this.major = major;
-    }
-    public void addFriend(String id){
-    	friends.put(id, id);
-    }
-    public void removeFriend(String id){
-    	friends.remove(id, id);
-    }
-    public String getId() {
-        return id;
-    }
+	private String id;
+	/** The unique user Id */
+	private String name;
+	/** The unique user Id */
+	private String major;
+	/** The timestamp when the user is being created */
+	private String creationTime = new Date(System.currentTimeMillis()).toString();
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	private HashMap friends;
+	private HashMap blackList;
+	private ArrayList mail;
+	private ArrayList sent;
+
+	public User() {
+		friends = new HashMap<String, String>();
+		blackList = new HashMap<String, String>();
+		mail = new ArrayList<Message>();
+		sent = new ArrayList<Message>();
+	}
+
+	public User(String id, String name, String major) {
+		friends = new HashMap<String, String>();
+		blackList = new HashMap<String, String>();
+		mail = new ArrayList<Message>();
+		sent = new ArrayList<Message>();
+		this.id = id;
+		this.name = name;
+		this.major = major;
+	}
+
+	public void addFriend(String id) {
+		friends.put(id, id);
+	}
+
+	public void removeFriend(String id) {
+		friends.remove(id, id);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name){
+	public void setName(String name) {
 		this.name = name;
 	}
 
@@ -63,8 +75,34 @@ public class User {
 	public String getCreationTime() {
 		return creationTime;
 	}
-
+	
 	public void setCreationTime(String creationTime) {
 		this.creationTime = creationTime;
 	}
+	/**
+	 *  this method will be used to write mail to this instance of user
+	 */
+	public boolean sendMail(String id, String message){
+		if(blackList.containsKey(id)){
+			return false;
+		}
+		
+		Message msg = new Message(id, message);
+		mail.add(msg);
+		
+		return true;
+	}
+	
+	public boolean writeMail(User recipient, String message){
+		if(recipient.blackList.containsKey(id)){
+			return false;
+		}
+		
+		Message msg = new Message(id, message);
+		recipient.mail.add(msg);
+		sent.add(msg);
+		
+		return true;
+	}
+
 }
