66afec637bc8459c53bb5319a4c3518bdd6f7020
Thomas Nguyen <tlnguyen1@cpp.edu>
2016-07-18 19:16:50 -0700

    Need to test this on ec2 client
    Might need some help using free marker to make a form that accepts a
    file

--- src/main/java/edu/csupomona/cs480/controller/WebController.java
+++ src/main/java/edu/csupomona/cs480/controller/WebController.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
@@ -29,6 +32,7 @@ import com.amazonaws.services.simpledb.model.ListDomainsRequest;
 import com.amazonaws.services.simpledb.model.ListDomainsResult;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
 
@@ -543,7 +547,45 @@ public class WebController extends WebMvcConfigurerAdapter {
 	}
 	
 	
-
+	//TODO Need to test this method with free marker
+	/**
+	 * Test function to see if an image can be uploaded to ec2 client, idk how to 
+	 * use free marker though to create something that accepts a file
+	 * @param image
+	 */
+	@RequestMapping(value = "/uploadImageTest", method = RequestMethod.POST)
+	void transferImageTest(File file)throws IOException{
+		int width = 100;
+		int height = 100;
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		image = ImageIO.read(file);
+		String BASE_DIR = System.getProperty("user.home") + "/images/database";
+		file = new File(BASE_DIR + "/" + "test.jpg");
+		ImageIO.write(image, "jpg", file);
+	}
+	
+	@RequestMapping(value = "/upload/{userId}", method = RequestMethod.POST)
+	void uploadUserImage(File file, @PathVariable("userId") String userId)throws IOException{
+		int width = 100;
+		int height = 100;
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		image = ImageIO.read(file);
+		String BASE_DIR = System.getProperty("user.home") + "/images/database";
+		file = new File(BASE_DIR + "/" + userId +".jpg");
+		ImageIO.write(image, "jpg", file);
+	}
+	
+	@RequestMapping(value = "/user/profile/{userId}", method = RequestMethod.GET)
+	BufferedImage getUserImage(@PathVariable("userId") String userId) throws IOException{
+		int width = 100;
+		int height = 100;
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		File f = ResourceResolver.getUserImage(userId);
+		image = ImageIO.read(f);
+		
+		return image;		
+	}
+	
 
 	@RequestMapping(value = "/user/{userId}", method = RequestMethod.GET)
 	ModelAndView getUser(@PathVariable("userId") String userId) {
