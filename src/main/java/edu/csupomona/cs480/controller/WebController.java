e0e764890ebfc5ea605befe90b36572ed7edebfd
henryh1570 <freeteame@gmail.com>
2016-07-19 19:42:51 -0700

    Cleaned up WebController.java unused files.

--- src/main/java/edu/csupomona/cs480/controller/WebController.java
+++ src/main/java/edu/csupomona/cs480/controller/WebController.java
@@ -87,35 +87,6 @@ public class WebController extends WebMvcConfigurerAdapter {
 	private GroupManager groupManager;
 	
 	/**
-	 * Cs480 - Assignment 3, part 3
-	 * Template Method by Henry Hu.
-	 */
-	@RequestMapping(value = "/cs480/temptest", method = RequestMethod.GET)
-	String templateMethod() {
-		return "This is a test message. Success!";
-		
-	}
-	
-	/**
-	 * Cs480 - Assignment 4, part 2 Prototype FileUpload Method by Henry Hu. The
-	 * following method is NOT COMPLETE. DO NOT RUN this method. The user will
-	 * provide a DiskFileItem which is a compacted upload file as an object. The
-	 * write method will write the uploaded file into the disk, our server.
-	 * 
-	 * @throws Exception
-	 */
-	@RequestMapping(value = "/cs480/user/filespace/upload", method = RequestMethod.GET)
-	String fileUploadMethod() throws Exception {
-
-		// parseUploadRequest()
-		// processUploadedItemsList()
-		// ------Make the DiskFileItem object--------
-		// dfi.write(dfi.getStoreLocation());
-		// FileCleanUp()
-		return "Testing the file upload classes";
-	}
-	
-	/**
 	 * Thomas Nguyen
 	 * Assignment 4 part 2
 	 * Please do not uncomment the awsTest method, currently missing some vital parts of aws
@@ -201,71 +172,7 @@ public class WebController extends WebMvcConfigurerAdapter {
             System.out.println("Error Message: " + ace.getMessage());
         }
     }
-		
 	
-	
-	/**
-	 *  CS480 - Assignment 3 
-	 *  Kevin Liu
-	 */
-	@RequestMapping(value = "/cs480/WeAreNotThatBad", method = RequestMethod.GET)
-	String notThatBadMethod() {
-		return "We're not that bad";
-	}
-	
-	/**
-	 * CS480 - Assignment 4
-	 * Kevin Liu 
-	 */
-	@RequestMapping(value = "/print_test", method = RequestMethod.GET)
-		String printer() throws Exception{
-		Fraction f = new Fraction(3.4);
-		return f.toString();
-	}
-	/**
-	*  CS 480 Assignment 4, part 2
-	*  jsoup test
-	*  Mark Ilog
-	*/
-	@RequestMapping(value = "/cs480/jsoupTest", method = RequestMethod.GET)
-        String jsoupTest() throws Exception {
-        Document doc;
-        try {
-     
-            String something = "";
-            // need http protocol
-            doc = Jsoup.connect("http://google.com").get();
-     
-            // get page title
-            something = something + "Title: " + doc.title() + "\n";
-     
-            // get all links
-            Elements links = doc.select("a[href]");
-            for (Element link : links) {
-     
-                // get the value from href attribute
-                something = something + "\nlink : " + link.attr("href");
-                something = something + "\ntext : " + link.text();
- 
-                return something;
-            }
-     
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "broken";
-        }
-        return "broken";
-    }
-	/**
-	 * cs480 - Assignment 3
-	 * Method by Mark Ilog.
-	 */
-
-	@RequestMapping(value = "/cs480/marktest", method = RequestMethod.GET)
-	String markMethod() {
-		return "Hello World";
-	}
-
 	/**
 	 * This is a simple example of how the HTTP API works.
 	 * It returns a String "OK" in the HTTP response.
@@ -280,92 +187,6 @@ public class WebController extends WebMvcConfigurerAdapter {
 		// with the URL: http://localhost:8080/
 		return "OK";
 	}
-
-	/**
-	 * This is a simple example of how to use a data manager
-	 * to retrieve the data and return it as an HTTP response.
-	 * <p>
-	 * Note, when it returns from the Spring, it will be
-	 * automatically converted to JSON format.
-	 * <p>
-	 * Try it in your web browser:
-	 * 	http://localhost:8080/cs480/user/user101
-	 */
-	/**@RequestMapping(value = "/cs480/user/{userId}", method = RequestMethod.GET)
-	User getUser(@PathVariable("userId") String userId) {
-		User user = userManager.getUser(userId);
-		return user;
-	}*/
-
-	/**
-	 * This is an example of sending an HTTP POST request to
-	 * update a user's information (or create the user if not
-	 * exists before).
-	 *
-	 * You can test this with a HTTP client by sending
-	 *  http://localhost:8080/cs480/user/user101
-	 *  	name=John major=CS
-	 *
-	 * Note, the URL will not work directly in browser, because
-	 * it is not a GET request. You need to use a tool such as
-	 * curl.
-	 *
-	 * @param id
-	 * @param name
-	 * @param major
-	 * @return
-	 */
-	/**@RequestMapping(value = "/cs480/user/{userId}", method = RequestMethod.POST)
-	User updateUser(
-			@PathVariable("userId") String id,
-			@RequestParam("name") String name,
-			@RequestParam(value = "major", required = false) String major) {
-		User user = new User(id, name, major);
-		userManager.updateUser(user);
-		return user;
-	}
-
-	/**
-	 * This API deletes the user. It uses HTTP DELETE method.
-	 *
-	 * @param userId
-	 */
-	/**@RequestMapping(value = "/cs480/user/{userId}", method = RequestMethod.DELETE)
-	void deleteUser(
-			@PathVariable("userId") String userId) {
-		userManager.deleteUser(userId);
-	}*/
-
-	/**
-	 * This API lists all the users in the current database.
-	 *
-	 * @return
-	 */
-	/**@RequestMapping(value = "/cs480/users/list", method = RequestMethod.GET)
-	List<User> listAllUsers() {
-		return userManager.listAllUsers();
-	}*/
-	
-	/**
-	 * This is a test method for part 3 of assignment 3.
-	 * 
-	 * @return
-	 */
-	/**@RequestMapping(value = "/cs480/testing", method = RequestMethod.GET)
-	String doSomething() {
-		return "something";
-	}*/
-	
-	
-	/**
-	 * Trying to implement a login page
-	 * Currently not working as clicking the login button leads to an error 
-	 */
-	/**@RequestMapping(value = "/cs480/login", method = RequestMethod.GET)
-	ModelAndView addViewControllers() {
-		ModelAndView modelAndView = new ModelAndView("login");
-        return modelAndView;
-	}*/
 	
 	/**
 	 * Uploads the file that contains all the users, but I don't
@@ -386,25 +207,7 @@ public class WebController extends WebMvcConfigurerAdapter {
 		userManager.downloadMap();
 		return "redirect:/";
 	}
-	
-	
-	/*********** Web UI Test Utility **********/
-	/**
-	 * This method provide a simple web UI for you to test the different
-	 * functionalities used in this web service.
-	 */
-	/**@RequestMapping(value = "/cs480/home", method = RequestMethod.GET)
-	ModelAndView getUserHomepage() {
-		ModelAndView modelAndView = new ModelAndView("home");
-		modelAndView.addObject("users", listAllUsers());
-		return modelAndView;
-	}*/
-	
-	/***
-	 *Testing actual pages
-	 *
-	 */
-	
+		
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	ModelAndView getLogin() {
 		ModelAndView modelAndView = new ModelAndView("login");
@@ -547,9 +350,6 @@ public class WebController extends WebMvcConfigurerAdapter {
 		return modelAndView;
 	}
 	
-	
-	
-	
 	@RequestMapping(value = "/search/major/{userId}", method = RequestMethod.GET)	
 	List<User> searchByMajor(@PathVariable("userId") String userId){
 		List<User> results = new ArrayList<User> ();
@@ -614,7 +414,6 @@ public class WebController extends WebMvcConfigurerAdapter {
 		return image;		
 	}
 	
-
 	@RequestMapping(value = "/user/{userId}", method = RequestMethod.GET)
 	ModelAndView getUser(@PathVariable("userId") String userId) {
 		User user = userManager.getUser(userId);
